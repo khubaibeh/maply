67b0921cfea3d69e6fe0fada180fd2218fe1c0b5
Khubaib Bin Ehsan <khubaib@khubaibeh.com>
2026-06-27 15:53:32 +0500

    chore: add effect and setups

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import { fileURLToPath } from "node:url";
+
 import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+const appPath = fileURLToPath(new URL("./app", import.meta.url));
+
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@app": appPath
+		}
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
