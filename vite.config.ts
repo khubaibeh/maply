a02359620802b8da51d2f0b545cc511d307fb096
Khubaib Bin Ehsan <khubaib@khubaibeh.com>
2026-06-29 01:32:26 +0500

    chore: move app alias to svelte config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { fileURLToPath } from "node:url";
-
-import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-const appPath = fileURLToPath(new URL("./app", import.meta.url));
-
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@app": appPath
-		}
-	},
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => (filename.split(/[/\\]/).includes("node_modules") ? undefined : true)
-			},
-			adapter: adapter()
-		})
-	]
+	plugins: [tailwindcss(), sveltekit()]
 });
