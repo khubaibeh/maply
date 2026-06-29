685d49708ca21068568b0563b8abef908517437d
Khubaib Bin Ehsan <khubaib@khubaibeh.com>
2026-06-30 00:55:10 +0500

    refactor(ui): split left sidebar sections

--- src/components/LeftSidebar.svelte
+++ src/components/LeftSidebar.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	import Sidebar from "$lib/components/app/panels/LeftSidebar.svelte";
+	import ElementsSection from "@components/left-sidebar/ElementsSection.svelte";
+	import ImportExportSection from "@components/left-sidebar/ImportExportSection.svelte";
+	import ProjectSection from "@components/left-sidebar/ProjectSection.svelte";
 
-	let { width }: { width: number } = $props();
+	let { width = 240 }: { width?: number } = $props();
 </script>
 
-<Sidebar {width} />
+<aside class="border-border bg-sidebar flex shrink-0 flex-col border-r" style={`width: ${width}px;`}>
+	<ProjectSection />
+	<ImportExportSection />
+	<ElementsSection />
+</aside>
